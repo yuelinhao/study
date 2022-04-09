abe4d2c257a1fc93d0d06c0ce9eb60d4ba3ff483
yuelinhao <2062318011@qq.com>
2022-04-09 23:12:49 +0800

    node.js

---
+++ Node.js/day3/code/08.使用express.static对外提供静态资源.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// 在这里，调用 express.static() 方法，快速的对外提供静态资源
+app.use('/files', express.static('./files'))
+app.use(express.static('./clock'))
+
+app.listen(80, () => {
+    console.log('express server running at http://127.0.0.1')
+})
+
+
+/* const express = require('express')
+const app1 = express()
+app.use(express.static('./clock'))
+app1.listen(80, () => {
+    console.log('express server running at http://127.0.0.1')
+}) */
